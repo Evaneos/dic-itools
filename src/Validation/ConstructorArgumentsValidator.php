f4f614d8f4646d060c1d05ce01e30141b9a7a546
Thibaud Fabre <thibaud@evaneos.com>
2014-02-09 14:49:16 +0100

    Add constructor parameter count validation

--- src/Validation/ConstructorArgumentsValidator.php
+++ src/Validation/ConstructorArgumentsValidator.php
@@ -14,6 +14,26 @@ class ConstructorArgumentsValidator implements ConfigValidator
         if ($class !== null) {
             if (! class_exists($class) && ! interface_exists($class)) {
                 $validator->addError('Class not found : ' . $class);
+                return;
+            }
+
+            $reflectionClass = new \ReflectionClass($class);
+
+            /* @var $reflectionCtor \ReflectionMethod */
+            $reflectionCtor = $reflectionClass->getConstructor();
+            $ctorArgs = $serviceNode->resolve('args', array());
+
+            if ($reflectionCtor == null) {
+                if( ! empty($args)) {
+                    $validator->addWarning('Constructor arguments are provided, but no constructor was found.');
+                }
+
+                return;
+            }
+
+            if (count($ctorArgs) < $reflectionCtor->getNumberOfRequiredParameters()) {
+                $validator->addError(sprintf('Invalid parameter count : %d instead of %d required.',
+                    count($ctorArgs), $reflectionCtor->getNumberOfParameters()));
             }
         }
     }
