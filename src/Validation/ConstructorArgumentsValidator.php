68a3e1e382897f55b46b801215b1ec48778fd63c
Thibaud Fabre <thibaud@evaneos.com>
2014-02-10 09:35:42 +0100

    Add constructor args type validation

--- src/Validation/ConstructorArgumentsValidator.php
+++ src/Validation/ConstructorArgumentsValidator.php
@@ -21,10 +21,10 @@ class ConstructorArgumentsValidator implements ConfigValidator
 
             /* @var $reflectionCtor \ReflectionMethod */
             $reflectionCtor = $reflectionClass->getConstructor();
-            $ctorArgs = $serviceNode->resolve('args', array());
+            $ctorArgs = $serviceNode->resolve('arguments', array());
 
             if ($reflectionCtor == null) {
-                if (! empty($args)) {
+                if (! empty($ctorArgs)) {
                     $validator->addWarning('Constructor arguments are provided, but no constructor was found.');
                 }
 
@@ -34,8 +34,60 @@ class ConstructorArgumentsValidator implements ConfigValidator
             if (count($ctorArgs) < $reflectionCtor->getNumberOfRequiredParameters()) {
                 $validator->addError(
                     sprintf('Invalid parameter count : %d instead of %d required.', count($ctorArgs),
+                        $reflectionCtor->getNumberOfRequiredParameters()));
+            }
+            elseif (count($ctorArgs) > $reflectionCtor->getNumberOfParameters()) {
+                $validator->addWarning(
+                    sprintf('Invalid parameter count : %d instead of %d defined.', count($ctorArgs),
                         $reflectionCtor->getNumberOfParameters()));
             }
+
+            /* @var $parameter \ReflectionParameter */
+            $i = 0;
+            $args = $ctorArgs->extract();
+            foreach ($reflectionCtor->getParameters() as $parameter) {
+                if ($i >= count($args)) {
+                    break;
+                }
+
+                $hint = $this->getHint($reflectionCtor->getDocComment(), $parameter->getName());
+                $type = $this->resolveType($global, $args[$i]);
+
+                if ($type != 'mixed' && $type != $hint && $hint != array_pop(explode('\\', $type))) {
+                    $validator->addWarning(
+                        sprintf('Parameter type mismatch : %s instead of %s defined.', $type,
+                            $hint));
+                }
+
+                $i++;
+            }
         }
+    }
+
+    private function resolveType(ArrayResolver $global, $reference)
+    {
+        $prefix = substr($reference, 0, 1);
+        if ($prefix == '@') {
+            return $global->resolve('classes.' . substr($reference, 1) . '.class');
+        }
+        else {
+            return 'mixed';
+        }
+    }
+
+    // Regex
+    private function getHint($docComment, $varName)
+    {
+        $matches = array();
+        $count = preg_match_all('/@param[\t\s]*(?P<type>[^\t\s]*)[\t\s]*\$(?P<name>[^\t\s]*)/sim', $docComment,
+            $matches);
+        if ($count > 0) {
+            foreach ($matches['name'] as $n => $name) {
+                if ($name == $varName) {
+                    return $matches['type'][$n];
+                }
+            }
+        }
+        return null;
     }
 }
--- src/Validation/DependencyValidator.php
+++ src/Validation/DependencyValidator.php
@@ -8,8 +8,33 @@ class DependencyValidator implements ConfigValidator
 
     public function validateService(Validator $validator, ArrayResolver $global, $serviceName, ArrayResolver $serviceNode)
     {
-        $this->validateNode($validator, $global, $serviceNode->resolve('args', array()));
-        $this->validateNode($validator, $global, $serviceNode->resolve('props', array()));
+        $this->validateNode($validator, $global, $serviceNode->resolve('arguments', array()));
+
+        $className = $serviceNode->resolve('class');
+        $properties = $serviceNode->resolve('props', array());
+        $this->validateNode($validator, $global, $properties);
+
+        if ($className == '\stdClass' || ! class_exists($className)) {
+            return;
+        }
+
+        $reflectionClass = new \ReflectionClass($className);
+        foreach ($properties as $propertyName => $propertyValue) {
+            if (property_exists($className, $propertyName)) {
+                continue;
+            }
+
+            if (method_exists($className, '__set')) {
+                $validator->addWarning(
+                    sprintf("Undefined target property'%s', but a magic set method was found.",
+                        $propertyName));
+            }
+            else {
+                $validator->addError(
+                    sprintf("Undefined target property'%s'.",
+                        $propertyName));
+            }
+        }
     }
 
     private function validateNode(Validator $validator, ArrayResolver $global, ArrayResolver $node)
